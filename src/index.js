0e09fb1aa335651dcddc35744e87bfd1fd008d8a
Deyis Karakas <deyiskarakas7@gmail.com>
2023-01-31 22:10:27 +0100

    edited

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ import "./assets/images/deyis.jpg";
 import "./assets/images/memory.jpeg";
 import "./assets/images/stock.jpeg";
 import "./assets/images/market.jpeg";
+import "./assets/images/rentacar.jpg";
